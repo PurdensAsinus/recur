b1103b36ffda47dc05fe4b5d174da44b4489656f
PurdensAsinus <PurdensAsinus@users.noreply.github.com>
2018-10-25 15:13:10 -0400

    Added header file

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int reverse(int n);
+
+int main() 
+{
+    int n;
+    cout<<"Enter a number to reverse ";
+    cin >> n;
+    cout << "The reverse of " << n <<" = " << reverse(n);
+    return 0;
+}
+
+int reverse(int n) 
+{ 
+ 
+}
